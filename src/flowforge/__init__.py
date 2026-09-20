f3d2df4f4067e8931c996652671d437f4cfe1fdb
yanni98 <93472547+yanni98@users.noreply.github.com>
2026-09-20 20:15:27 +0100

    Create __init__.py

---
+++ src/flowforge/__init__.py
@@ -0,0 +1,12 @@
+"""
+FlowForge - lightweight asynchronous workflows for Python.
+"""
+
+from .workflow import Workflow, WorkflowResult
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Workflow",
+    "WorkflowResult",
+]
